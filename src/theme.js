45b24d3d5ef28c62317d8c42777dbaab614d1dee
AdityaRawEngg <aditya.shukla@raweng.com>
2021-02-24 12:17:07 +0530

    Create a theme for  the app

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,59 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-export const theme = createMuiTheme({});
+export const theme = createMuiTheme({
+  typography: {
+    h1: {
+      display: "block",
+      fontSize: "2em",
+      marginTop: "0.67em",
+      marginBottom: "0.67em",
+      marginLeft: "0",
+      marginRight: "0",
+      fontWeight: "bold",
+    },
+    h2: {
+      display: "block",
+      fontSize: "1.5em",
+      marginTop: "0.83em",
+      marginBottom: "0.83em",
+      marginLeft: "0",
+      marginRight: "0",
+      fontWeight: "bold",
+    },
+    h3: {
+      display: "block",
+      fontSize: "1.17em",
+      marginTop: "1em",
+      marginBottom: "1em",
+      marginLeft: "0",
+      marginRight: "0",
+      fontWeight: "bold",
+    },
+    h4: {
+      display: "block",
+      marginTop: "1.33em",
+      marginBottom: "1.33em",
+      marginLeft: "0",
+      marginRight: "0",
+      fontWeight: "bold",
+    },
+    h5: {
+      display: "block",
+      fontSize: "0.83em",
+      marginTop: "1.67em",
+      marginBottom: "1.67em",
+      marginLeft: "0",
+      marginRight: "0",
+      fontWeight: "bold",
+    },
+    h6: {
+      display: "block",
+      fontSize: "0.67em",
+      marginTop: "2.33em",
+      marginBottom: "2.33em",
+      marginLeft: "0",
+      marginRight: "0",
+      fontWeight: "bold",
+    },
+  },
+});
