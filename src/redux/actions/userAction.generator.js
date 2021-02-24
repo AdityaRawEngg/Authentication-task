7c0a6e02efd0ebf59bfb8a6fbaa2ac751d54eac1
AdityaRawEngg <aditya.shukla@raweng.com>
2021-02-24 12:11:39 +0530

    added new action for error

--- src/redux/actions/userAction.generator.js
+++ src/redux/actions/userAction.generator.js
@@ -1,12 +1,57 @@
 import { userActionTypes } from "../constants/userAction.types";
+import { apiCall } from "../../axios/index";
+import { endpoint } from "../../axios/endpoints";
+import { getCookie, setCookie } from "../../helper/cookies";
 
 const userActionGenerator = (actionTypes, payload = {}) => {
   switch (actionTypes) {
     case userActionTypes.ADD:
+      return async (dispatch) => {
+        const { data } = await apiCall({
+          url: endpoint.login,
+          body: { ...payload },
+          method: "POST",
+        });
+        setCookie("Token", data.token, {
+          path: "/",
+          expires: new Date(Date.now() + 86400e3 * 2),
+        });
+        dispatch({
+          type: userActionTypes.ADD,
+          payload: { user: { ...data.user } },
+        });
+      };
+    case userActionTypes.GET:
+      return (dispatch) => {
+        apiCall({
+          url: endpoint.userDetails,
+          method: "GET",
+          headers: { Authorization: `Bearer ${getCookie("Token")}` },
+        })
+          .then((response) => {
+            dispatch({
+              type: userActionTypes.GET,
+              payload: {
+                user: { ...response.data.user },
+              },
+            });
+          })
+          .catch((err) => {
+            dispatch({
+              type: userActionTypes.GET,
+              payload: {
+                ...payload,
+              },
+            });
+          });
+      };
+
+    case userActionTypes.ERROR:
       return {
-        type: userActionTypes.ADD,
+        type: userActionTypes.ERROR,
         payload: { ...payload },
       };
+
     default:
       return {
         type: "Invalid",
