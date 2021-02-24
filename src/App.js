71fa3f9c78fa18e78cb45b5ef885279de070eae3
AdityaRawEngg <aditya.shukla@raweng.com>
2021-02-24 12:08:16 +0530

    Added a theme and CssBaseLine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Provider } from "react-redux";
 import Router from "./routes";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
 // import logo from "./logo.svg";
 import store from "./redux/store/store";
 import "./App.css";
+import { theme } from "./theme";
 
 function App() {
   return (
     <Provider store={store}>
-      <Router />
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router />
+      </MuiThemeProvider>
     </Provider>
   );
 }
