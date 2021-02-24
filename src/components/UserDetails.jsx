52c6f70a6ad7c009fa1139355fa9e8d9b87c9aca
AdityaRawEngg <aditya.shukla@raweng.com>
2021-02-24 12:21:13 +0530

    Display user details

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -2,16 +2,16 @@ import { Box, Typography } from "@material-ui/core";
 export default function UserDetails({ user }) {
   return (
     <Box>
-      <Typography variant="h3" align="center" color="textSecondary">
+      <Typography variant="h2" align="center" color="textSecondary">
         Profile
       </Typography>
-      <Typography variant="h5" align="center" color="textSecondary">
+      <Typography variant="h3" align="center" color="textSecondary">
         Name: {user.name}
       </Typography>
-      <Typography variant="h5" align="center" color="textSecondary">
+      <Typography variant="h3" align="center" color="textSecondary">
         Email: {user.email}
       </Typography>
-      <Typography variant="h6" align="center" color="textSecondary">
+      <Typography variant="h3" align="center" color="textSecondary">
         Phone: {user.phone}
       </Typography>
     </Box>
