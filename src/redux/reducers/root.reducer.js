15a164f358a7f7b847ed10be2aa7273e70a6e6e4
AdityaRawEngg <aditya.shukla@raweng.com>
2021-02-24 12:15:22 +0530

    combine alert reducer to root reducer

--- src/redux/reducers/root.reducer.js
+++ src/redux/reducers/root.reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import userReducer from "./users.reducer";
+import alertReducer from "./alert.reducer";
 
 const rootReducer = combineReducers({
   userReducer: userReducer,
+  alertReducer: alertReducer,
 });
 export default rootReducer;
